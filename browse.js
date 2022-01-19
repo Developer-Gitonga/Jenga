c6a837bbd445306fb4e6cd35eee5f5f102967aee
Paul Munyao <paulmunyao094@gmail.com>
2022-01-19 04:36:07 +0300

    Set transformation for the next image

--- browse.js
+++ browse.js
@@ -1,12 +1,23 @@
 startImageTransition();
-function startImageTransition(){
-    const images = document.getElementsByClassName("test");
+function startImageTransition() {
+  const images = document.getElementsByClassName("test");
 
-    for (const i = 0; i < images.length; i++) {
-        images[i].style.opacity = 1;
-    }
+  for (const i = 0; i < images.length; i++) {
+    images[i].style.opacity = 1;
+  }
 
-    const top =1;
-    const cur = images.length - 1;
-    setInterval(changeImage, 3000);
+  const top = 1;
+
+  const cur = images.length - 1;
+
+  setInterval(changeImage, 3000);
+
+  async function changeImage() {
+      const nextImage = (1 + cur) % images.length;
+
+      images[cur].style.zIndex =top +1;
+      images[nextImage].style.zIndex = top;
+
+      await transition();
+  }
 }
