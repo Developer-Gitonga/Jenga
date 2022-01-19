4d4c35d80c7d6d9c6172f28cb63fd0e790579d97
Paul Munyao <paulmunyao094@gmail.com>
2022-01-19 04:31:16 +0300

    Set image transition to 3000 milisecond

--- browse.js
+++ browse.js
@@ -5,4 +5,8 @@ function startImageTransition(){
     for (const i = 0; i < images.length; i++) {
         images[i].style.opacity = 1;
     }
+
+    const top =1;
+    const cur = images.length - 1;
+    setInterval(changeImage, 3000);
 }
