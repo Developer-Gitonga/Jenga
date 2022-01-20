d2d8327582c9dd84711975b8928da4b7d643a435
FadumoM <fadumomohamed011@gmail.com>
2022-01-20 23:13:54 +0300

    login js

--- login.js
+++ login.js
@@ -1,30 +1,27 @@
-function validateInput(){
-  var emailValid=document.getElementById('typeEmail').value;
+ function validateInput(){
+ var emailValid=document.getElementById('typeEmail').value;
   var passwordValid=document.getElementById('typePassword').value;
 
   console.log(passwordValid);
-//    if(email== ""){
-//      alert("Section cannot be left blank");
-//      return false;
-//    }
-//    elseif(password.length<8){
-//       alert("Password should be 8 character");
-//       return false;
-//    }
-//    else{
-//      return true;
-//    }
-// };
+  
+  //if(emailValid == null || emailValid== ""){
+   // alert("Section cannot be left blank");
+  //}else {
+   // return false;
+  //}
+  //if(passwordValid == "" || passwordValid.length < 8){
+   // alert("Minimum length of password should be 8 characters");
+ // }
+ // else{
+  //  return false;
+ // }
+//}
 
-  if(emailValid == ""){
-    alert("Section cannot be left blank");
-  }else {
-    alert("Enter Password")
-  }
-  if(passwordValid == "" || passwordValid.length < 8){
-    alert("Minimum length of password should be 8 characters");
-  }
-  else{
-    return true;
-  }
-}
+if(emailValid == null || emailValid ==""){
+alert("Section cannot be left empty");
+return false;
+};
+if(passwordValid.length <8);
+alert("password should be 8 characaters")
+return false;
+ };
